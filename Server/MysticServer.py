17606f5c53e2d8809871e1d206fbf5d738f259b0
kobiza1 <kobiza@post.bgu.ac.il>
2024-04-14 17:32:26 +0300

    finish server, most parts checked

--- Server/MysticServer.py
+++ Server/MysticServer.py
@@ -3,29 +3,104 @@ import time
 import random
 import socket
 import struct
-import ipaddress
+import psutil
+
+# ANSI reset
+RESET = "\033[0m"
+
+# ANSI escape codes for formatting
+BOLD = "\033[1m"
+
+# ANSI escape codes for background colors
+PALE_BLUE_BACKGROUND = "\033[104m"
+BACKGROUND_BLUE = "\033[44m"
+BACKGROUND_YELLOW = "\033[43m"
+PURPLE_BACKGROUND = "\033[45m"
+RED_BACKGROUND = "\033[41m"
+WHITE_BACKGROUND = "\033[107m"
+ORANGE_BACKGROUND = "\033[48;2;255;165;0m"
+CYAN_BACKGROUND = "\033[46m"
+
+# ANSI escape codes for colors
+RED_TEXT = "\033[91m"
+GREEN_TEXT = "\033[92m"
+YELLOW_TEXT = "\033[93m"
+WHITE_TEXT = "\033[97m"
+BLACK_TEXT = "\033[30m"
 
 
-INTERFACE_NAME = '192.168.1.220'
-UDP_PORT = 13117
-TCP_PORT = 54574
+
+RIGHT_ANSWER = 'You showed great knowledge you must be a pro!'
+WRONG_ANSWER = 'Wrong answer maybe next time.'
+INVALID_ANSWER = 'Answer have to be one of the following: y, t, 1 for True or n, f, 0 for False'
+NO_ANSWER = 'Ohh you missed it, You have to answer faster!'
+NO_GOOD_ANSWERS = 'You can do better. 0/20 really!?'
+END_OF_GAME = 'Thank you for playing, hope to see you soon! :)'
+WELCOME_MESSAGE = 'Welcome to our NBA and Surfing quiz.\nYou better be right if you want to win!'
 BUFFER_SIZE = 1024
 MAGIC_COOKIE = 0xabcddcba
 MESSAGE_TYPE = 0x2
 TIME_OUT_IN_SEC = 10
+WIFI_INTERFACE_NAMES = [
+    # Linux
+    "wlan0", "wlan1", "wlan2",  # Traditional naming convention
+    # Newer Predictable Network Interface Names
+    "wlpXsY",
+    # macOS
+    "Wi-Fi",
+    # Windows
+    "Wi-Fi", "Wireless Network Connection", "WLAN",
+    # BSD
+    "wlan0", "wlan1", "wlan2"
+]
+POSSIBLE_TRUE_ANSWERS = ['y', 't', '1']
+POSSIBLE_FALSE_ANSWERS = ['0', 'n', 'f']
+
+
+def find_available_port(tcp_or_udp):
+
+    start_port = 49152
+    end_port = 65535
+
+    for port in range(start_port, end_port + 1):
+        if tcp_or_udp and UDP_PORT == port:
+            continue
+        sock = None
+        try:
+            # Create a UDP socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            # Try binding to the current port
+            sock.bind(('localhost', port))
+            # If successful, return the port
+            print(f'port {port} is free to use.')
+            return port
+        except OSError:
+            # Port is already in use, try the next one
+            pass
+        finally:
+            # Close the socket
+            sock.close()
+
+    # If no available port is found
+    return None
+
+
+UDP_PORT = find_available_port(False)
+TCP_PORT = find_available_port(True)
+
 
 # List of trivia questions about NBA
 nba_questions = [
-    {"question": "Michael Jordan won six NBA championships with the Chicago Bulls.", "is_true": True},
-    {"question": "The Los Angeles Lakers have won the most NBA championships in history.", "is_true": True},
-    {"question": "LeBron James has won the NBA Most Valuable Player (MVP) award five times.", "is_true": True},
-    {"question": "The Golden State Warriors set the record for most wins in an NBA regular season with 73 wins.", "is_true": True},
-    {"question": "Kobe Bryant scored 100 points in a single NBA game.", "is_true": False},
-    {"question": "The NBA was founded in 1950.", "is_true": False},
-    {"question": "Larry Bird played his entire NBA career with the Boston Celtics.", "is_true": True},
-    {"question": "The NBA Slam Dunk Contest was first held in 1976.", "is_true": True},
-    {"question": "The NBA three-point line was introduced in the 1980s.", "is_true": False},
-    {"question": "Shaquille O'Neal was known for his exceptional three-point shooting.", "is_true": False}
+    {"question": RED_BACKGROUND + BLACK_TEXT + "Michael Jordan won six NBA championships with the Chicago Bulls." + RESET, "is_true": True},
+    {"question": PURPLE_BACKGROUND + YELLOW_TEXT + "The Los Angeles Lakers have won the most NBA championships in history." + RESET, "is_true": True},
+    {"question": RED_BACKGROUND + WHITE_TEXT + "LeBron James has won the NBA Most Valuable Player (MVP) award five times." + RESET, "is_true": True},
+    {"question": BACKGROUND_BLUE + YELLOW_TEXT + "The Golden State Warriors set the record for most wins in an NBA regular season with 73 wins." + RESET, "is_true": True},
+    {"question": PURPLE_BACKGROUND + YELLOW_TEXT + "Kobe Bryant scored 100 points in a single NBA game." + RESET, "is_true": False},
+    {"question": ORANGE_BACKGROUND + BLACK_TEXT + "The NBA was founded in 1950." + RESET, "is_true": False},
+    {"question": WHITE_BACKGROUND + GREEN_TEXT + "Larry Bird played his entire NBA career with the Boston Celtics." + RESET, "is_true": True},
+    {"question": ORANGE_BACKGROUND + BLACK_TEXT + "The NBA Slam Dunk Contest was first held in 1976." + RESET, "is_true": True},
+    {"question": ORANGE_BACKGROUND + BLACK_TEXT + "The NBA three-point line was introduced in the 1980s." + RESET, "is_true": False},
+    {"question": PURPLE_BACKGROUND + YELLOW_TEXT + "Shaquille O'Neal was known for his exceptional three-point shooting." + RESET, "is_true": False}
 ]
 
 # List of trivia questions about surfing
@@ -42,26 +117,29 @@ surfing_questions = [
     {"question": "Surfing was first practiced by ancient Polynesians.", "is_true": True}
 ]
 
+
+def format_surfing_questions():
+    for q in surfing_questions:
+        q['question'] = CYAN_BACKGROUND + '~~~' + q['question'] + '~~~' + RESET
+
+
+format_surfing_questions()
 ALL_QUESTIONS = nba_questions + surfing_questions
 
 
-def get_ip_address(interface):
-    # Get the IP address of the specified interface
-    try:
-        ip_address = socket.getaddrinfo(interface, None, socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_IP)[0][4][0]
-        return str(ip_address)
-    except OSError:
-        return None
-
-
-def get_subnet_mask(ip_address):
-    try:
-        ip_network = ipaddress.ip_interface(ip_address)
-        subnet_mask_length = ip_network.network.prefixlen
-        subnet_mask = ipaddress.IPv4Network('0.0.0.0/' + str(subnet_mask_length)).netmask
-        return str(subnet_mask)
-    except (ValueError, AttributeError):
-        return None
+def get_wireless_ip_address():
+    interfaces = psutil.net_if_addrs()
+    for interface_name, interface_addresses in interfaces.items():
+        cleaned_name = interface_name.replace("\u200F\u200F", "")
+        for interface in WIFI_INTERFACE_NAMES:
+            if cleaned_name.startswith(interface):
+                for address in interface_addresses:
+                    if address.family == socket.AF_INET:
+                        ip_address = address.address
+                        subnet_mask = address.netmask
+                        return ip_address, subnet_mask
+    print("Cannot get wireless IP address")
+    return None, None
 
 
 def calculate_broadcast_ip(ip_address, subnet_mask):
@@ -75,8 +153,11 @@ def calculate_broadcast_ip(ip_address, subnet_mask):
         return None
 
 
-SERVER_IP = get_ip_address(INTERFACE_NAME)
-print(SERVER_IP)
+SERVER_IP, SUBNET_MASK = get_wireless_ip_address()
+if not SERVER_IP or not SUBNET_MASK:
+    print('There is no available wifi network, try again later')
+    exit(0)
+
 
 
 def create_tcp_socket():
@@ -101,15 +182,23 @@ def check_answer(answer, correct_answer):
     return answer == correct_answer
 
 
-
-def calc_broadcast_ip(interface):
-
-    ip_address = get_ip_address(interface)
-    subnet_mask = get_subnet_mask(interface)
-    print(ip_address, subnet_mask)
+def calc_broadcast_ip():
 
     # Calculate the broadcast IP address
-    return calculate_broadcast_ip(ip_address, subnet_mask)
+    return calculate_broadcast_ip(SERVER_IP, SUBNET_MASK)
+
+
+def add_to_locked_list(player, my_lock, list_to_add):
+    my_lock.acquire()
+    try:
+        list_to_add.append(player)
+    finally:
+        my_lock.release()
+
+
+def send_message(client_socket, message):
+    client_socket.sendall(message.encode('utf-8'))
+    print(message)
 
 
 class Server:
@@ -120,20 +209,20 @@ class Server:
         self.UDP_socket = create_udp_socket()
         self.TCP_socket = create_tcp_socket()
         self.clients = []
-        # self.ip_address = get_ip_address(INTERFACE_NAME)
         self.timer = 0
         self.timer_lock = threading.Lock()
         self.clients_lock = threading.Lock()
         self.answers = []
         self.answers_lock = threading.Lock()
-
+        self.have_winner_lock = threading.Lock()
+        self.have_winner = []
 
     def send_offer_broadcast(self, message, broadcast):
 
         while self.timer < TIME_OUT_IN_SEC:
             self.UDP_socket.sendto(message, (broadcast, UDP_PORT))
             print(message)
-            print("Offer broadcasted")
+            print("Offer broadcast")
             self.add_to_timer()
             time.sleep(1)
 
@@ -144,16 +233,7 @@ class Server:
         player_name = client_sock.recv(1024).decode('utf-8').strip()
         print(f"Received player name: {player_name}")
         player_tup = (client_sock, client_add, player_name)
-        self.add_to_locked_list(player_tup, self.clients_lock)
-
-
-    def add_to_locked_list(self, player, my_lock):
-        my_lock.acquire()
-        try:
-            self.clients.append(player)
-        finally:
-            my_lock.release()
-
+        add_to_locked_list(player_tup, self.clients_lock, self.clients)
 
     def accept_clients(self):
 
@@ -170,6 +250,11 @@ class Server:
             except socket.timeout:
                 print("Timeout reached. No more clients will be accepted.")
                 break
+
+        if len(self.clients) == 0:
+            print('There is no players right now!')
+            exit(0)
+
 
 
     def add_to_timer(self):
@@ -195,7 +280,7 @@ class Server:
         return offer_packet
 
 
-    def get_answer(self, client_socket, player_name):
+    def get_answer(self, client_socket, player_name, correct_ans):
 
         client_socket.settimeout(TIME_OUT_IN_SEC)
 
@@ -203,58 +288,48 @@ class Server:
 
             player_ans = client_socket.recv(BUFFER_SIZE)
             str_player_ans = player_ans.decode('utf-8')
-            player_tup = (player_name, str_player_ans)
-            self.add_to_locked_list(player_tup, self.answers_lock)
+            answer = self.get_bool_ans(str_player_ans)
+
+            if check_answer(answer, correct_ans):
+                player = (player_name, client_socket)
+                add_to_locked_list(player, self.have_winner_lock, self.have_winner)
+                send_message(client_socket, RIGHT_ANSWER)
+            elif answer is None:
+                send_message(client_socket, INVALID_ANSWER)
+            else:
+                send_message(client_socket, WRONG_ANSWER)
 
         except socket.timeout:
             print("Timeout reached. No more clients will be accepted.")
+            send_message(client_socket, NO_ANSWER)
         except Exception as e:
             print("An error occurred while receiving data from the client:", e)
 
 
-    def check_answers(self, correct_ans):
-        for ans in self.answers:
-            if check_answer(ans[1], correct_ans):
-                return True
-        return False
-
-
     def start_game(self):
 
-        no_winner = True
         index = 0
         random.shuffle(ALL_QUESTIONS)
 
-        while no_winner:
-            message = ALL_QUESTIONS[index]["question"]
+        while len(self.have_winner) == 0 and index < len(ALL_QUESTIONS):
+
+            message = f'Question {index+1}, True or False: {ALL_QUESTIONS[index]["question"]}'
+            self.send_all(message)
             correct_ans = ALL_QUESTIONS[index]["is_true"]
             threads = []
+
             for client_socket, client_address, player_name in self.clients:
                 try:
-                    client_socket.sendall(message.encode('utf-8'))
-                    ans_thread = threading.Thread(target=self.get_answer, args=(client_socket, player_name))
+                    ans_thread = threading.Thread(target=self.get_answer, args=(client_socket, player_name, correct_ans))
                     ans_thread.start()
                     threads.append(ans_thread)
                     print("Message sent to client at:", client_address)
                 except Exception as e:
                     print("Failed to send message to client at:", client_address, "Error:", e)
 
-            # TODO check if we got good answer
-
             for thread in threads:
                 thread.join()
-
-            if not self.check_answers(correct_ans):
-                continue
-
-
-
             index += 1
-
-
-
-
-
 
 
 
@@ -262,15 +337,54 @@ class Server:
 
         print(f"Server started, listening on IP address {SERVER_IP}")
         offer_message = self.construct_offer_packet()
-        broadcast_ip = calc_broadcast_ip(INTERFACE_NAME)
+        broadcast_ip = calc_broadcast_ip()
         # Start broadcasting offer announcements in a separate thread
         offer_thread = threading.Thread(target=self.send_offer_broadcast, args=(offer_message, broadcast_ip))
         offer_thread.start()
         self.accept_clients()
-
+        self.send_welcome_message()
         self.start_game()
+        if len(self.have_winner):
+            self.announce_winner()
+        else:
+            self.send_all(NO_GOOD_ANSWERS)
+        self.disconnect_all()
+
+    @staticmethod
+    def get_bool_ans(str_player_ans):
+        lower_ans = str_player_ans.lower()
+        if lower_ans in POSSIBLE_TRUE_ANSWERS:
+            return True
+        elif lower_ans in POSSIBLE_FALSE_ANSWERS:
+            return False
+        return None
+
+    def announce_winner(self):
+        message = f'GAME OVER!\nCongratulations to the winner: {BOLD} {self.have_winner[0][0]}! {RESET}'
+        self.send_all(message)
+
+
+    def send_all(self, message):
+        for client_socket, _, _ in self.clients:
+            client_socket.sendall(message.encode('utf-8'))
+        print(message)
+
+    def disconnect_all(self):
+        self.send_all(END_OF_GAME)
+        for client_socket, _, _ in self.clients:
+            client_socket.close()
+        print('Game over, sending out offer requests...')
+        self.manage_game()
+
+    def send_welcome_message(self):
+        message = WELCOME_MESSAGE
+        i = 1
+        for _, _, player_name in self.clients:
+            message += f'\nPlayer {i}: {player_name}'
+        message += '\n==\n'
+        self.send_all(message)
 
 
 if __name__ == "__main__":
-    s = Server("kobiz")
+    s = Server("kibitz")
     s.manage_game()
